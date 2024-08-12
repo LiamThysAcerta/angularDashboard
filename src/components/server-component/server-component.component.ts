7bf69b7ee5d64ca13a30a89dfe95d7733e3a3585
LiamThys <liam.thys11@gmail.com>
2024-08-12 17:29:41 +0200

    add server-component

---
+++ src/components/server-component/server-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-component.component.html',
+  styleUrl: './server-component.component.css'
+})
+export class ServerComponentComponent {
+
+}
